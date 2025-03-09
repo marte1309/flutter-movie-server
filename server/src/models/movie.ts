0c739b26e02e4e04a36ba4bcebc2080d9759b8e3
Mariano Rodriguez <mrd1309@gmail.com>
2025-03-09 17:26:12 -0600

    server fixes

--- server/src/models/movie.ts
+++ server/src/models/movie.ts
@@ -1,5 +1,4 @@
-import { Movie } from '../types/movie';
+import { Movie } from "../types/movie";
 
 // Exportamos la variable movies para que pueda ser usada en otros módulos
 export const movies: Movie[] = [];
-export let nextId = 1;
