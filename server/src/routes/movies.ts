626f4212687f7c7b331b949ad4ef75145918b786
Mariano Rodriguez <mrd1309@gmail.com>
2025-03-08 21:37:16 -0600

    Añadir rutas para películas

---
+++ server/src/routes/movies.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import * as movieController from '../controllers/movieController';
+
+const router = express.Router();
+
+router.get('/', movieController.getAllMovies);
+router.get('/:id', movieController.getMovieById);
+router.post('/', movieController.addMovie);
+router.put('/:id', movieController.updateMovie);
+router.delete('/:id', movieController.deleteMovie);
+router.post('/scan', movieController.scanDirectory);
+
+export default router;
