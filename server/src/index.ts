62c66e32a1ffc71c9eea3298276dfad052d2b708
Mariano Rodriguez <mrd1309@gmail.com>
2025-03-12 10:51:53 -0600

    fix server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,6 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
-import http from "http";
-import https from "https";
-import fs from "fs";
-import config from "./config";
 import movieRoutes from "./routes/movies";
 import streamRoutes from "./routes/stream";
 
@@ -21,24 +17,7 @@ app.use("/api/stream", streamRoutes);
 // Servir archivos estáticos (para la página web de administración)
 app.use("/admin", express.static(path.join(__dirname, "..", "public")));
 
-// Configuración HTTPS
-const httpsOptions = {
-  key: fs.readFileSync(path.join(__dirname, "..", "certs", "key.pem")),
-  cert: fs.readFileSync(path.join(__dirname, "..", "certs", "cert.pem")),
-};
-
 // Inicia el servidor HTTP para streaming
-const httpServer = http.createServer(app);
-
-// Crear servidor HTTPS
-https.createServer(httpsOptions, app).listen(config.PORT, () => {
-  console.log(
-    `Servidor HTTPS ejecutándose en https://localhost:${config.PORT}`
-  );
-});
-
-httpServer.listen(config.PORT + 1, () => {
-  console.log(
-    `Servidor HTTP ejecutándose en http://localhost:${config.PORT + 1}`
-  );
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor HTTP iniciado en http://localhost:${process.env.PORT}`);
 });
